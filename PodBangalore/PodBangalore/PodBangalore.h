8152bd13db65e6ce2541dad5ab94c1b459562a41
Babul S Raj <babulsraj@Babuls-MacBook-Pro.local>
2025-01-01 19:53:24 +0530

    Added bangalore repo

---
+++ PodBangalore/PodBangalore/PodBangalore.h
@@ -0,0 +1,18 @@
+//
+//  PodBangalore.h
+//  PodBangalore
+//
+//  Created by Babul S Raj on 01/01/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PodBangalore.
+FOUNDATION_EXPORT double PodBangaloreVersionNumber;
+
+//! Project version string for PodBangalore.
+FOUNDATION_EXPORT const unsigned char PodBangaloreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PodBangalore/PublicHeader.h>
+
+
